f5a1dcdfbedebf630d62a4b586ed9a880d1458c5
krasimiranova <krasy_nova@yahoo.com>
2024-02-05 09:52:25 +0100

    fater queue

--- src/main/java/exercises/lab01LinearDataStructure_exercises/implementations/Queue.java
+++ src/main/java/exercises/lab01LinearDataStructure_exercises/implementations/Queue.java
@@ -1,4 +1,4 @@
-package lab.lab01LinearDataStructure.implementations;
+package implementations;
 
 import interfaces.AbstractQueue;
 
@@ -6,45 +6,45 @@ import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
     private Node<E> head;
-    private Node<E> tail;
     private int size;
+
     private static class Node<E> {
         private E element;
         private Node<E> next;
-        public Node(E value) {
-            this.element = value;
+
+        private Node(E element) {
+            this.element = element;
         }
     }
+
     public Queue() {
-        this.head = null;
-        this.size = 0;
     }
+
     @Override
     public void offer(E element) {
         Node<E> newNode = new Node<>(element);
         if (this.head == null) {
-            this.head = this.tail = newNode;
+            this.head = newNode;
         } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-
+            Node<E> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
         this.size++;
     }
 
     @Override
     public E poll() {
-        if(this.isEmpty()) {
-            throw new IllegalStateException();
-        }
-
+        ensureNonEmpty();
         E element = this.head.element;
         if (this.size == 1) {
-            this.head = this.tail = null;
+            this.head = null;
         } else {
-            Node<E> next = this.head.next; // new next node = 73(which is the next value after the head)
-            this.head.next = null; // the next value after the head (the top value got cut off) = null(because... ->)
-            this.head = next; // ...the head = 73(the new top value)
+            Node<E> next = this.head.next;
+            this.head.next = null;
+            this.head = next;
         }
         this.size--;
         return element;
@@ -52,10 +52,7 @@ public class Queue<E> implements AbstractQueue<E> {
 
     @Override
     public E peek() {
-        if(this.isEmpty()) {
-            throw new IllegalStateException();
-        }
-
+        ensureNonEmpty();
         return this.head.element;
     }
 
@@ -73,6 +70,7 @@ public class Queue<E> implements AbstractQueue<E> {
     public Iterator<E> iterator() {
         return new Iterator<E>() {
             private Node<E> current = head;
+
             @Override
             public boolean hasNext() {
                 return this.current != null;
@@ -81,11 +79,15 @@ public class Queue<E> implements AbstractQueue<E> {
             @Override
             public E next() {
                 E element = this.current.element;
-
                 this.current = this.current.next;
-
                 return element;
             }
         };
+    }
+
+    private void ensureNonEmpty() {
+        if (this.size == 0) {
+            throw new IllegalStateException("Illegal operation on empty stack");
+        }
     }
 }
--- src/main/java/lab/lab01LinearDataStructure/implementations/SinglyLinkedList.java
+++ src/main/java/lab/lab01LinearDataStructure/implementations/SinglyLinkedList.java
@@ -1,24 +1,62 @@
-package implementations;
+package lab.lab01LinearDataStructure.implementations;
 
 import interfaces.LinkedList;
 
 import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private int size;
 
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E value) {
+            this.element = value;
+        }
+    }
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
     @Override
     public void addFirst(E element) {
+        Node<E> nextElement = new Node<>(element);
 
+        nextElement.next = this.head;
+        this.head = nextElement;
+
+        this.size++;
     }
 
     @Override
     public void addLast(E element) {
+        Node<E> lastElement = new Node<>(element);
 
+        if(this.isEmpty()) {
+            this.head = lastElement;
+        } else {
+            Node<E> current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = lastElement;
+        }
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        if(this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        E element = this.head.element;
+        this.head = this.head.next;
+        this.size--;
+        return element;
     }
 
     @Override
@@ -28,7 +66,10 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
 
     @Override
     public E getFirst() {
-        return null;
+        if(this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        return this.head.element;
     }
 
     @Override
@@ -38,12 +79,12 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
