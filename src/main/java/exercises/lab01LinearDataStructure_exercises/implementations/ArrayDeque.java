c55e5c79a6325c8aea1a65d2efb138c8131e8111
krasimiranova <krasy_nova@yahoo.com>
2024-02-06 20:00:49 +0100

    ArrayDeque

--- src/main/java/exercises/lab01LinearDataStructure_exercises/implementations/ArrayDeque.java
+++ src/main/java/exercises/lab01LinearDataStructure_exercises/implementations/ArrayDeque.java
@@ -1,13 +1,51 @@
-package implementations;
+package exercises.lab01LinearDataStructure_exercises.implementations;
 
 import interfaces.Deque;
 
+import javax.swing.text.Element;
 import java.util.Iterator;
 
 public class ArrayDeque<E> implements Deque<E> {
+    private final int INITIAL_CAPACITY = 7;
+    private int size;
+    private int head;
+    private int tail;
+    private Object[] elements;
+    public ArrayDeque() {
+        this.elements = new Object[INITIAL_CAPACITY];
+        int middle = INITIAL_CAPACITY / 2;
+        head = tail = middle;
+    }
     @Override
-    public void add(E Element) {
+    public void add(E element) {
+        if(this.size == 0) {
+            this.elements[this.tail] = element;
+        } else {
+            if(this.tail == this.elements.length - 1) {
+                this.elements = grow();
+            }
+            this.elements[++this.tail] = element;
+        }
+        this.size++;
+    }
 
+    private Object[] grow() {
+        int newCapacity = this.elements.length * 2 + 1;
+
+        Object[] newElements = new Object[newCapacity];
+
+        int middle = newCapacity / 2;
+        int begin = middle - this.size / 2;
+        int index = this.head;
+
+        for (int i = begin; index <= this.tail; i++) {
+            newElements[i] = this.elements[index++];
+        }
+
+        this.head = begin;
+        this.tail = this.head + this.size - 1;
+
+        return newElements;
     }
 
     @Override
